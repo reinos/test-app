501a7d352d544b93825dfff48e151b17f79416e0
Rein de Vries <info@reinos.nl>
2019-11-05 12:02:47 +0100

    add start script

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.listen(80, function () {
+app.listen(8080, function () {
     console.log('Just a test');
 });
