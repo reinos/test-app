1bada369060b361f4463452da67059e88b484335
Rein <info@reinos.nl>
2019-11-06 09:07:57 +0100

    Change port

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.listen(8080, function () {
+app.listen(80, function () {
     console.log('Just a test');
 });
